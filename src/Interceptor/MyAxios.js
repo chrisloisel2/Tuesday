537fa2e986b48582e113fefeb44311d1665baafd
ChristopherLoisel <christo.loisel@gmail.com>
2025-03-05 19:09:22 +0100

    ok

--- src/Interceptor/MyAxios.js
+++ src/Interceptor/MyAxios.js
@@ -5,10 +5,6 @@ const MyAxios = axios.create({
 	// baseURL: "http://localhost:8080/",
 	headers: {
 		"Content-Type": "application/json",
-		Accept: "application/json",
-		"Access-Control-Allow-Origin": "*",
-		"Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-		"Access-Control-Allow-Headers": "Content-Type, Authorization",
 	},
 	withCredentials: true,
 });
