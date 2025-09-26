3004fe29681401b52f4b42412f64d970b11c0690
ChristopherLoisel <christo.loisel@gmail.com>
2025-09-26 23:34:26 +0200

    ok

--- src/data/users.js
+++ src/data/users.js
@@ -1,14 +1,32 @@
 export const authorizedUsers = [
-        {
-                username: "admin",
-                password: "password123",
-                boardUrl: "https://embed.monday.com/embed/boards/4201786698",
-                boardLabel: "Pilotage - Admin",
-        },
-        {
-                username: "manager",
-                password: "monday!2024",
-                boardUrl: "https://embed.monday.com/embed/boards/5112768042?view=timeline",
-                boardLabel: "Suivi - Manager",
-        },
+	{
+		username: "pablo",
+		password: "password123",
+		boardUrl: "https://view.monday.com/2005977860-aeaa3bbab705b013ab620f0f20ad927f?r=euc1",
+		boardLabel: "Pilotage - Pablo",
+	},
+	{
+		username: "redouane",
+		password: "password123",
+		boardUrl: "https://view.monday.com/2005977860-361177a26e8cc6c8e6ffb5929a062aae?r=euc1",
+		boardLabel: "Pilotage - Redouane",
+	},
+	{
+		username: "adam",
+		password: "password123",
+		boardUrl: "https://view.monday.com/2005977860-a343355404ff50476549f6a13e99892e?r=euc1",
+		boardLabel: "Pilotage - Adam",
+	},
+	{
+		username: "hachem",
+		password: "password123",
+		boardUrl: "https://view.monday.com/2005977860-04e9ffdc4b3efda44c770a48ecf52e52?r=euc1",
+		boardLabel: "Pilotage - Hachem",
+	},
+	{
+		username: "jalal",
+		password: "password123",
+		boardUrl: "https://view.monday.com/2005977860-1351be764db46e3c1dfcc5be9843a46e?r=euc1",
+		boardLabel: "Pilotage - Jalal",
+	},
 ];
